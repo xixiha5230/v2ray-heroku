b9efa02159ccf8a11dba2f8ccca8bef318a18957
xixiha <xixiha5230@gmail.com>
2021-06-16 02:17:18 +0800

    go back v2ray

--- configure.sh
+++ configure.sh
@@ -2,14 +2,16 @@
 
 mkdir /tmp
 cd /tmp
-git clone https://github.com/xixiha5230/web-heroku.git
-install -m 755 /tmp/web-heroku/web /usr/local/bin/web
+git clone https://github.com/xixiha5230/v2ray-heroku
+install -m 755 /tmp/v2ray-heroku/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray-heroku/v2ctl /usr/local/bin/v2ctl
 
-# Remove temporary directory
-rm -rf /tmp/web-heroku
+rm -rf /tmp/v2ray-heroku
 
-install -d /usr/local/etc/web
-cat << EOF > /usr/local/etc/web/config.json
+install -d /usr/local/etc/v2ray
+install -d /usr/local/etc/v2ctl
+
+cat << EOF > /usr/local/etc/v2ray/config.json
 {
     "inbounds": [
         {
@@ -39,7 +41,6 @@ cat << EOF > /usr/local/etc/web/config.json
     ]
 }
 EOF
-#cat /usr/local/etc/web/config.json
-# Run web
-/usr/local/bin/web -config /usr/local/etc/web/config.json
-rm /usr/local/etc/web/config.json
+
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+rm /usr/local/etc/v2ray/config.json
